f17e43987cc55ec9b0ab6a0f5b820886b8af7e62
NotTheJerry <quinterodiego208@gmail.com>
2024-04-21 13:30:48 -0600

    Principio de Inmutabilidad

--- src/main/java/com/gera/springboot/di/app/springbootdi/controllers/SomeController.java
+++ src/main/java/com/gera/springboot/di/app/springbootdi/controllers/SomeController.java
@@ -14,15 +14,17 @@ import com.gera.springboot.di.app.springbootdi.services.ProductService;
 @RequestMapping("/api")
 public class SomeController {
 
-    ProductService service = new ProductService();
+    private ProductService service = new ProductService();
 
     @GetMapping("/")
     public List<Product> list() {
+        ProductService service = new ProductService();
         return service.findAll();
     }
 
     @GetMapping("/{id}")
     public Product show( @PathVariable Long id ) {
+        ProductService service = new ProductService();
         return service.findById(id);
     }
 }
