563912151edd55f28a72b5c025ceae961e4e435c
NotTheJerry <quinterodiego208@gmail.com>
2024-04-21 21:04:46 -0600

    Creando clase que lee los datos de un archivo json

---
+++ src/main/java/com/gera/springboot/di/app/springbootdi/repositories/ProductRepositoryJson.java
@@ -0,0 +1,38 @@
+package com.gera.springboot.di.app.springbootdi.repositories;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gera.springboot.di.app.springbootdi.models.Product;
+
+public class ProductRepositoryJson implements ProductRepository {
+
+    private List<Product> list;
+
+    public ProductRepositoryJson () {
+        Resource resource = new ClassPathResource("json/product.json");
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            list = Arrays.asList(objectMapper.readValue(resource.getFile(), Product[].class));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return list;
+    }
+
+    @Override
+    public Product findById(Long id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
